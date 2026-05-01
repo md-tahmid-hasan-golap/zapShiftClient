494f0e869fa9fb5cda862c354d5d7728b39ac698
md-tahmid-hasan-golap <golapraj47@gmail.com>
2026-05-01 15:26:19 +0600

    resolve

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -1,7 +1,8 @@
 const Services = () => {
   return (
-    <div>
+    <div className="my-20 text-center">
       <h2>this is Services</h2>
+      <button className="btn btn-primary">Login </button>
     </div>
   );
 };
