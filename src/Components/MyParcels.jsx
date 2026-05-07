bbdbd0b936d561252bc6307c33d445d7d92fc2f8
md-tahmid-hasan-golap <golapraj47@gmail.com>
2026-05-08 00:27:54 +0600

    resolve

--- src/Components/MyParcels.jsx
+++ src/Components/MyParcels.jsx
@@ -1,7 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import { useContext } from "react";
+import { AuthContext } from "../firebase/FirebaseAuthProvider";
+import UseAxiussecure from "./UseAxiussecure";
+
 const MyParcels = () => {
+  const { user } = useContext(AuthContext);
+  const axiusSecure = UseAxiussecure();
+  const { data: parcels = [] } = useQuery({
+    queryKey: ["myParcels", user?.email],
+    queryFn: async () => {
+      const res = await axiusSecure.get(`/myParcel/${user?.email}`);
+      return res.data;
+    },
+  });
+
   return (
     <div>
-      <h2>This is My Parcels</h2>
+      <h2>This is My Parcels {parcels.length}</h2>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./Router/Router.jsx";
 import FirebaseAuthProvider from "./firebase/FirebaseAuthProvider.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <FirebaseAuthProvider>
-      <RouterProvider router={router}></RouterProvider>
-    </FirebaseAuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <FirebaseAuthProvider>
+        <RouterProvider router={router}></RouterProvider>
+      </FirebaseAuthProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
